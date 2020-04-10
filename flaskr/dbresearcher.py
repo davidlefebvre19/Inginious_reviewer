818558956f9937c6dc0be588a8f2b168c101b054
laptop <davidlfbvr19@gmail.com>
2020-04-10 20:29:41 +0200

    function of sql graph search

---
+++ flaskr/dbresearcher.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+
+
+def list_of_tuples_id_tasks_with_succes_number_sorted():
+	conn = sqlite3.connect('schema.sqlite')
+	cursor = conn.cursor()
+
+	succes_frequency = {}
+
+	for row in cursor.execute("SELECT DISTINCT task FROM submissions"):
+		succes_frequency[row[0]] = None
+	for row in cursor.execute("SELECT task, result FROM submissions WHERE result LIKE '%succes%'"):
+		if succes_frequency[row[0]] == None:
+			succes_frequency[row[0]] = 1
+		else :
+			succes_frequency[row[0]] += 1
+	conn.close()
+	sorted_succes_frequency = sorted(succes_frequency.items(), key = lambda kv:(kv[1], kv[0]))
+	return sorted_succes_frequency
